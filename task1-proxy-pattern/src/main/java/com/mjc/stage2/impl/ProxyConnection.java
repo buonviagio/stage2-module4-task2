11497a8da0da56c1773123256cecdefb9dde02d8
Shapovalov Dmytro <buonviagio@gmail.com>
2023-07-12 15:27:31 +0200

    My First Commit Stage#2 Module#4 Task#2

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -1,15 +1,33 @@
 package com.mjc.stage2.impl;
 
 
-public class ProxyConnection {
+import com.mjc.stage2.Connection;
+
+public class ProxyConnection implements Connection {
     private RealConnection realConnection;
 
     public ProxyConnection(RealConnection realConnection) {
+
         this.realConnection = realConnection;
     }
 
     public void reallyClose() {
         // Write your code here!
+        // my implementation 10.07
+        realConnection.close();
     }
+
     // Implement methods here!
+    @Override
+    public void close() {
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        connectionPool.releaseConnection(realConnection);
+
+    }
+    // my implementation 10.07
+    @Override
+    public boolean isClosed() {
+        return realConnection.isClosed();
+        //return false;
+    }
 }
